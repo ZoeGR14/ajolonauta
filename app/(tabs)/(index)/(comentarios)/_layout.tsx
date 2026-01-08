330acdbbb1a507829ec9d72ce2a5c4d2182c666c
ZoeGR14 <zgonzalezr2001@alumno.ipn.mx>
2026-01-08 17:13:08 -0600

    Modificacion Stack comentarios

--- app/(tabs)/(index)/(comentarios)/_layout.tsx
+++ app/(tabs)/(index)/(comentarios)/_layout.tsx
@@ -1,11 +1,10 @@
 import { Stack } from "expo-router";
 
 export default function IndexLayout() {
-  return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="leerAvisos" />
-      <Stack.Screen name="crearAviso" />
-      <Stack.Screen name="avisoTwitter" />
-    </Stack>
-  );
+   return (
+      <Stack screenOptions={{ headerShown: false }}>
+         <Stack.Screen name="leerAvisos" />
+         <Stack.Screen name="crearAviso" />
+      </Stack>
+   );
 }
