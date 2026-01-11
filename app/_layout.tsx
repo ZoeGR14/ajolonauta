e27614ed47627817bc5ad50877b23d2527347cf5
WilliamsZeppeli <jquintanarb2000@alumno.ipn.mx>
2026-01-11 16:59:06 -0600

    diseño final

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,51 +1,54 @@
 import {
-   Poppins_400Regular,
-   Poppins_700Bold,
-   useFonts,
+  Poppins_400Regular,
+  Poppins_700Bold,
+  useFonts,
 } from "@expo-google-fonts/poppins";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
-import { useCallback } from "react";
-import { Platform, StatusBar as RNStatusBar, Text, View } from "react-native";
+import { useEffect } from "react";
+import { Platform, View, StatusBar as RNStatusBar } from "react-native";
 
+// Mantiene el splash screen visible mientras cargamos recursos
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-   const [fontsLoaded] = useFonts({
-      Poppins_400Regular,
-      Poppins_700Bold,
-   });
+  const [fontsLoaded, error] = useFonts({
+    Poppins_400Regular,
+    Poppins_700Bold,
+  });
 
-   const onLayoutRootView = useCallback(async () => {
-      if (fontsLoaded) {
-         await SplashScreen.hideAsync();
-      }
-   }, [fontsLoaded]);
+  // Manejo de errores y ocultamiento del Splash Screen
+  useEffect(() => {
+    if (error) throw error;
 
-   if (fontsLoaded && !(Text as any).defaultProps) {
-      (Text as any).defaultProps = {
-         style: { fontFamily: "Poppins_400Regular" },
-      };
-   }
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
 
-   if (!fontsLoaded) return null;
+  // Si las fuentes no han cargado y no hay error, no renderizamos nada aún
+  if (!fontsLoaded && !error) {
+    return null;
+  }
 
-   return (
-      <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
-         {/* Simula el fondo del status bar */}
-         <View
-            style={{
-               height:
-                  Platform.OS === "android" ? RNStatusBar.currentHeight : 44,
-               backgroundColor: "#e68059",
+  return (
+    <View style={{ flex: 1 }}>
+      {/* StatusBar 'light' pone las letras en blanco (para fondos oscuros).
+         Si tu fondo es blanco, cámbialo a style="dark".
+      */}
+      <StatusBar style="dark" translucent />
+       <View 
+         style={{
+               height: Platform.OS === "android" ? RNStatusBar.currentHeight : 44,
+
+               backgroundColor: "#DC2626",
             }}
          />
-         <StatusBar style="light" />
-         <Stack screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="(auth)" />
-            <Stack.Screen name="(tabs)" />
-         </Stack>
-      </View>
-   );
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(auth)" />
+        <Stack.Screen name="(tabs)" />
+      </Stack>
+    </View>
+  );
 }
